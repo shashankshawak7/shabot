9565fab7d3c463ceae8901ccf0b05227008593d9
shashank shawak <shashankshawak7@gmail.com>
2017-11-02 03:27:34 +0530

    Update shabot.py

--- shabot.py
+++ shabot.py
@@ -1,8 +1,3 @@
-#from zzz import talking_mode
-#from zzz import learning_mode
-#from server_engine import server_startup
-import socket
-import time
 
 
 #!/usr/bin/python3
@@ -16,6 +11,8 @@ import sys
 from chatterbot.trainers import ListTrainer
 from chatterbot.conversation import statement
 import socket
+import time
+
 #import Threading
 chatterbot = ChatBot(
     'shabot',
@@ -32,33 +29,19 @@ chatterbot.set_trainer(ChatterBotCorpusTrainer)
 #CONVERSATION_ID = chatterbot.storage.create_conversation()
 def talking_mode(input_statements,CONVERSATION_ID):
         #input_statement = input_statements.decode()
-        input_statement = input_statements
-        #print (input_statement)
-    #print ('Type something to begin...')
-
-    # The following loop will execute each time the user enters input
-
-         #while True:
-                #try:
-        #input_statement = input_statements.decode()
-       # CONVERSATION_ID = chatterbot.storage.create_conversation()
+        input_statement = input_statement
         input_statement = chatterbot.input.process_input_statement(input_statement)
         print (input_statement)
         statement, response = chatterbot.generate_response(input_statement, CONVERSATION_ID)
-                #print (CONVERSATION_ID)
+                
         output_statement=chatterbot.output.process_response(response)
         return output_statement
-                #print(output_statement)
-               # bot_input = chatterbot.get_response(None)
-                #except (KeyboardInterrupt, EOFError,SystemExit):
-                # Press ctrl-c or ctrl-d on the keyboard to exit
-                 #       break
-
+               
 
 
 #####################################################################################################################
 
-
+#main function to run the server 
 
 #import socket
 def main():
